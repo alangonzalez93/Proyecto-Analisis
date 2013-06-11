6963fbe8762f5a2a571b46e7687f1b20ff4bee92
jheredia <joaquin.heredia93@gmail.com>
2013-06-10 22:56:58 -0300

    Update on Main

--- inmo/src/main/java/com/unrc/app/Inmo.java
+++ inmo/src/main/java/com/unrc/app/Inmo.java
@@ -1,9 +1,15 @@
 package com.unrc.app;
 import static spark.Spark.*;
+
+import com.unrc.app.ABMBuilding;
+import com.unrc.app.ABMOwner;
+import com.unrc.app.ABMRealEstate;
+
+
 import spark.*;
 
-import org.javalite.activejdbc.Base;
-import org.javalite.activejdbc.LazyList;
+import org.javalite.activejdbc.*;
+
 
 import com.unrc.app.models.Building;
 import com.unrc.app.models.Locality;
@@ -13,10 +19,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.*;
+import java.io.*;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import freemarker.template.Configuration;
+
+
+
 
 @SuppressWarnings("unused")
 public class Inmo {
-	private static Scanner sc =new Scanner(System.in);
+	
+	
     public static void main( String[] args )
     {
         Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "root");
@@ -84,17 +100,157 @@ public class Inmo {
         buil.altaBuilding("banda norte", 2000, "alquiler", "casa", "banda norte", "cordoba 661", "rio cuarto", "37546231");
         buil.altaBuilding("barrio alberdi", 3500, "alquiler", "casa", "alberdi", "jujuy 451", "achiras", "37546231");
         
-        Search prueba = new Search();
-        final String cadena = prueba.toString();
-        System.out.println(cadena);
+        Search search = new Search();
+        final String cadena = search.toString();
         
-        get(new Route("/buildings") {
-            @Override
-            public Object handle(Request request, Response response) {
-               return ("Inmuebles"+cadena);
-            }
-         });
-        
-        System.out.println( "Hello World!" );
+        	get(new Route("/buildings") {
+               @Override
+               public Object handle(Request request, Response response) {
+                  return "Inmuebles: "+cadena;
+               }
+            });
+        	
+       
+        	get(new Route("/index") {
+                @Override
+               
+                public Object handle(Request request, Response response) {
+       			//Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "Pantera1.");
+                   String title = "<h1> Inmobiliaria </h1>";
+
+       			String menu = "<ul> <li> <a href=\"/building\"> Inmuebles </a> </li> </ul>";
+       			String menu2 = "<ul> <li> <a href=\"/owner\"> Dueños </a> </li> </ul>";
+       			String menu3 = "<ul> <li> <a href=\"/realEstate\"> Inmobiliarias </a> </li> </ul>";
+
+       			//Base.close();
+                   return title + menu + menu2 + menu3;
+                };
+       		 });
+        	
+        	get(new Route("/building") {
+                @Override
+               
+                public Object handle(Request request, Response response) {
+       			//Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "Pantera1.");
+                   String title = "<h1>Menú Inmueble </h1>";
+
+       			String alta = "<ul> <li> <a href=\"/altaInmueble\"> Crear inmueble </a> </li> </ul>";
+       			String baja = "<ul> <li> <a href=\"/bajaInmueble\"> Eliminar inmueble </a> </li> </ul>";
+       			String listar  = "<ul> <li> <a href=\"/listarInmueble\"> Listar inmuebles </a> </li> </ul>";
+       			String atras = "<ul> <li> <a href=\"/index\">Volver</a></li></ul>";
+
+       			//Base.close();
+                   return title + alta + baja + listar + atras;
+                };
+       		 });
+        	
+        	get(new Route("/owner") {
+                @Override
+               
+                public Object handle(Request request, Response response) {
+       			//Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "Pantera1.");
+                   String title = "<h1>Menú Dueños </h1>";
+
+       			String alta = "<ul> <li> <a href=\"/altaOwner\"> Crear dueño </a> </li> </ul>";
+       			String baja = "<ul> <li> <a href=\"/bajaOwner\"> Eliminar dueño </a> </li> </ul>";
+       			String listar  = "<ul> <li> <a href=\"/listarOwner\"> Listar dueños </a> </li> </ul>";
+       			String atras = "<ul> <li> <a href=\"/index\">Volver</a></li></ul>";
+
+       			//Base.close();
+                   return title + alta + baja + listar + atras;
+                };
+       		 });
+        	
+        	get(new Route("/realEstate") {
+                @Override
+               
+                public Object handle(Request request, Response response) {
+       			//Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/inmoapp_development", "root", "Pantera1.");
+                   String title = "<h1>Menú Inmobiliarias </h1>";
+
+       			String alta = "<ul> <li> <a href=\"/altaRS\"> Crear inmobiliaria </a> </li> </ul>";
+       			String baja = "<ul> <li> <a href=\"/bajaRS\"> Eliminar inmobiliaria </a> </li> </ul>";
+       			String listar  = "<ul> <li> <a href=\"/listarRS\"> Listar inmobiliarias </a> </li> </ul>";
+       			String atras = "<ul> <li> <a href=\"/index\">Volver</a></li></ul>";
+
+       			//Base.close();
+                   return title + alta + baja + listar + atras;
+                };
+       		 });
+        	
+            post(new Route("/owner") {
+              //  Random random = new Random();
+                @Override
+                public Object handle(Request request, Response response) {
+                    String name = request.queryParams("owner_name");
+                    String dni = request.queryParams("owner_dni");
+                    String neighborhood = request.queryParams("neighborhood");
+                    String street = request.queryParams("street");
+                    String mail = request.queryParams("mail");
+                    String locality = request.queryParams("locality");
+                    ABMOwner abmO = new ABMOwner();
+                    abmO.Alta(name, neighborhood, street, mail, locality, dni);
+                 //   int id = random.nextInt(Integer.MAX_VALUE);
+                  //  books.put(String.valueOf(id), book);
+                    
+                    response.status(201); // 201 Created
+                    return ("Dueño DNI: "+ dni +"exitosamente");
+                }
+            });
+            
+
+            get(new Route("/listarOwner") {
+            	Search search = new Search();
+            	final String owner = search.OwnerStr();
+            	@Override
+                public Object handle(Request request, Response response) {
+                   return "Dueños: "+ owner + "";
+                }
+             });
+            
+            get(new Route("/listarRS") {
+            	Search search = new Search();
+            	final String rs = search.RealEstates();
+            	@Override
+                public Object handle(Request request, Response response) {
+                   return "Inmobiliaria: "+ rs;
+                }
+             });
+
+            
+            get(new Route("/listarInmueble") {
+            	Search search = new Search();
+            	final String inmuebles = search.toString();
+            	@Override
+                public Object handle(Request request, Response response) {
+                   return "Inmuebles: "+inmuebles;
+                }
+             });
+            
+            
+            post(new Route("/alta") {
+            	@Override 
+            	public Object handle(Request request, Response response) {
+            		//(String description, int price, String category, String type, 
+            		//String neighborhood, String street, String locality, String dni) {
+            		String description = request.queryParams("description");
+            		Integer price = Integer.parseInt(request.queryParams("price"));
+            		String category = request.queryParams("category");
+            		String type = request.queryParams("type");
+            		String neighborhood = request.queryParams("neighborhood");
+            		String street = request.queryParams("street");
+            		String locality = request.queryParams("locality");
+            		String dni = request.queryParams("dni");
+            		ABMBuilding abmB = new ABMBuilding();
+            		abmB.altaBuilding(description, price, category, type, neighborhood, street, locality, dni);
+            		String volver =  "<ul> <li> <a href=\"/building\">Volver</a></li></ul>";
+            		
+            		response.status(201); // 201 Created
+                    return ("Inmueble creado exitosamente en la direccion: "+ street + "/br" +  "<ul> <li> <a href=\"/index/building\">Volver</a></li></ul>");
+            	}
+            });
+			
+            
+           System.out.println( "Hello World!" );
     }
 }
--- inmo/src/main/java/com/unrc/app/Search.java
+++ inmo/src/main/java/com/unrc/app/Search.java
@@ -12,6 +12,8 @@ import org.javalite.activejdbc.LazyList;
 
 import com.unrc.app.models.Building;
 import com.unrc.app.models.Locality;
+import com.unrc.app.models.Owner;
+import com.unrc.app.models.RealEstate;
 
 
 public class Search{
@@ -26,20 +28,65 @@ public class Search{
 	}
 	
 	public String toString(){
-		LazyList<Building> build = Building.findAll();  //where("price >= ? and price <= ? and type = ? and locality_id = ?",min,max,type,verifLocality(loc));
+		LazyList<Building> build = Building.findAll();//where("price >= ? and price <= ? and type = ? and locality_id = ?",min,max,type,verifLocality(loc));
 		int i=0;
 		long cant = build.size();
-		String list="";
+		String list = " ";
 		while ((!build.isEmpty()) && i < cant){
 			Building buil = build.get(i);
 			int id = buil.getInteger("id");
 			//System.out.println(id);
 			int price = buil.getInteger("price");
 			//System.out.println(price);
-			list= list+" precio: "+price+ " id: "+ id;
+			list= "\n" + list+" Precio: "+price+ " id: "+ id + "\n";
 			i++;
 		}
 		return list;
+	}
+	
+	public String RealEstates(){
+		LazyList<RealEstate> re = RealEstate.findAll();//where("price >= ? and price <= ? and type = ? and locality_id = ?",min,max,type,verifLocality(loc));
+		int i=0;
+		long cant = re.size();
+		String list="";
+		while ((!re.isEmpty()) && i < cant){
+			RealEstate realestate = re.get(i);
+			int id = realestate.getInteger("id");
+			String web = realestate.getString("web");
+			String mail = realestate.getString("rs_mail");
+			String name = realestate.getString("rs_name");
+			String neigh = realestate.getString("rs_neighborhood");
+			String street = realestate.getString("rs_street");
+			int tel = realestate.getInteger("tel");
+			int locality = realestate.getInteger("locality_id");
+			Locality loc = Locality.first("id = ?", locality);
+			String namelocality = loc.getString("name");
+			
+			list= "\n"+list+" Nombre: "+name+ " id: "+ id + " Localidad: "+ namelocality+ " Barrio: "+ neigh+ " Calle: "+ street+ " Telefono: "+ tel+ " Mail: "+mail+" Web: "+web +"\n";
+			i++;
+		}
+		return list;
+	}
+	
+	public String OwnerStr(){
+		LazyList<Owner> own = Owner.findAll(); //where fuck da shit
+		int i=0;
+		long cant = own.size();
+		String listOwn = "";
+		while ((!own.isEmpty()) && i< cant){
+			Owner ownie = own.get(i);
+			String name= ownie.getString("owner_name");
+			String mail= ownie.getString("owner_mail");
+			String neighborhood = ownie.getString("owner_neighborhood");
+			String street = ownie.getString("owner_street");
+			String dni = ownie.getString("owner_dni");
+			int locality = ownie.getInteger("locality_id");
+			Locality loc = Locality.first("id =?",locality);
+			String name_locality = loc.getString ("name");
+			listOwn = "\n"+listOwn+" Nombre: "+name+" E-mail: "+mail+" Localidad: "+name_locality+" Barrio: "+neighborhood+" Calle: "+street+" DNI: "+dni+"\n";
+			i++;
+		}
+		return listOwn;
 	}
 	
 	public LazyList<Building> search(int min, int max, int type, String loc){
